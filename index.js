c158fc207e6539da537962bcefc4a83634bcef88
Henry_DelGlitch <CodingWHenry@gmail.com>
2023-01-26 09:24:39 -0500

    Commented out VIP code in index.js

--- index.js
+++ index.js
@@ -87,89 +87,89 @@ async function eventRetrieveal() {
     }
 
     // Fetching Data from VIP Table in Airtable
-    try {
-        //Array to capture information from the VIP Table
-        let vipMembers = [];
-        //Saved the response to a variable
-        let vipResponse = await fetch(`https://api.airtable.com/v0/appf7G8225c26By1w/VIP?api_key=keyU9BHPSgikWgQC9`);
-        //Response will be converted into text
-        let vipText = await vipResponse.text();
-        //Splits the response text when there is a comma
-        if (vipText.endsWith(",")) vipText = vipText.slice(0, -1);
-        //Response text will be parsed
-        vipMembers = JSON.parse(vipText);
-        //Length of the captured table 
-        let vipLength = vipMembers.records.length;
-        //Getting span element for seats
-        let vipLengthText = document.getElementById('vip-seats-left');
-        if (50 - vipLength !== 1) {
-            vipLengthText.innerText = `Only ${50 - vipLength} spaces are left.`;
-        } else {
-            vipLengthText.innerText = `Only ${50 - vipLength} space are left.`;
-        }
-        //Change inner text of span
-        //The three buttons at the bottom of the page
-        const threeButtonsDiv = document.getElementById("three-buttons-div");
-        //Purple package div 
-        const purpleBoxDiv = document.getElementById("purple-box");
-        //Purple package button 
-        const vipBoxDiv = document.getElementById("vip-box");
+    // try {
+    //     //Array to capture information from the VIP Table
+    //     let vipMembers = [];
+    //     //Saved the response to a variable
+    //     let vipResponse = await fetch(`https://api.airtable.com/v0/appf7G8225c26By1w/VIP?api_key=keyU9BHPSgikWgQC9`);
+    //     //Response will be converted into text
+    //     let vipText = await vipResponse.text();
+    //     //Splits the response text when there is a comma
+    //     if (vipText.endsWith(",")) vipText = vipText.slice(0, -1);
+    //     //Response text will be parsed
+    //     vipMembers = JSON.parse(vipText);
+    //     //Length of the captured table 
+    //     let vipLength = vipMembers.records.length;
+    //     //Getting span element for seats
+    //     let vipLengthText = document.getElementById('vip-seats-left');
+    //     if (50 - vipLength !== 1) {
+    //         vipLengthText.innerText = `Only ${50 - vipLength} spaces are left.`;
+    //     } else {
+    //         vipLengthText.innerText = `Only ${50 - vipLength} space are left.`;
+    //     }
+    //     //Change inner text of span
+    //     //The three buttons at the bottom of the page
+    //     const threeButtonsDiv = document.getElementById("three-buttons-div");
+    //     //Purple package div 
+    //     const purpleBoxDiv = document.getElementById("purple-box");
+    //     //Purple package button 
+    //     const vipBoxDiv = document.getElementById("vip-box");
 
-        //Check if the length is over 50
-        if (vipLength >= 50) {
-            //Remove Reserve my space button
-            purpleBoxDiv.removeChild(purpleBoxDiv.childNodes[3]);
-            //Remove They'll go fast quote on the site.
-            vipBoxDiv.removeChild(vipBoxDiv.childNodes[11]);
-            //Change "Only 50 spaces are available" to "All 50 spaces sold out"
-            vipBoxDiv.childNodes[9].className = "vip-highlighted";
-            vipBoxDiv.childNodes[9].innerText = "All 50 spaces sold out";
-            //Button at the bottom will be change to this text
-            threeButtonsDiv.childNodes[3].innerText = "SOLD OUT!\nJOIN OUR WAITING LIST";
+    //     //Check if the length is over 50
+    //     if (vipLength >= 50) {
+    //         //Remove Reserve my space button
+    //         purpleBoxDiv.removeChild(purpleBoxDiv.childNodes[3]);
+    //         //Remove They'll go fast quote on the site.
+    //         vipBoxDiv.removeChild(vipBoxDiv.childNodes[11]);
+    //         //Change "Only 50 spaces are available" to "All 50 spaces sold out"
+    //         vipBoxDiv.childNodes[9].className = "vip-highlighted";
+    //         vipBoxDiv.childNodes[9].innerText = "All 50 spaces sold out";
+    //         //Button at the bottom will be change to this text
+    //         threeButtonsDiv.childNodes[3].innerText = "SOLD OUT!\nJOIN OUR WAITING LIST";
 
-            //Create a new button in the package div to lead to the waiting list
-            const button = document.createElement('a');
-            button.className = "btn btn-purple btn-cartoon btn-cartoon-shadow btn-fill";
-            button.href = "https://register.centercentre.com/metricsvipwaitlist";
-            button.target = "_blank";
+    //         //Create a new button in the package div to lead to the waiting list
+    //         const button = document.createElement('a');
+    //         button.className = "btn btn-purple btn-cartoon btn-cartoon-shadow btn-fill";
+    //         button.href = "https://register.centercentre.com/metricsvipwaitlist";
+    //         button.target = "_blank";
 
-            const div = document.createElement('div');
-            div.id = "regular-text";
-            div.className = "cta";
-            div.innerText = "JOIN OUR WAITING LIST";
+    //         const div = document.createElement('div');
+    //         div.id = "regular-text";
+    //         div.className = "cta";
+    //         div.innerText = "JOIN OUR WAITING LIST";
 
-            //The waiting list text is appendding to button
-            button.appendChild(div);
-            //Button appended to packageDiv
-            purpleBoxDiv.appendChild(button);
-        }
-    } catch (err) {
-        // //If it fails to catch the airtable data for VIP
+    //         //The waiting list text is appendding to button
+    //         button.appendChild(div);
+    //         //Button appended to packageDiv
+    //         purpleBoxDiv.appendChild(button);
+    //     }
+    // } catch (err) {
+    //     // //If it fails to catch the airtable data for VIP
 
-        // //The three buttons at the bottom of the page
-        // const threeButtonsDiv = document.getElementById("three-buttons-div");
-        // //Purple package div 
-        // const purpleBoxDiv = document.getElementById("purple-box");
+    //     // //The three buttons at the bottom of the page
+    //     // const threeButtonsDiv = document.getElementById("three-buttons-div");
+    //     // //Purple package div 
+    //     // const purpleBoxDiv = document.getElementById("purple-box");
 
-        // //Remove Reserve my space button
-        // purpleBoxDiv.removeChild(purpleBoxDiv.childNodes[3]);
+    //     // //Remove Reserve my space button
+    //     // purpleBoxDiv.removeChild(purpleBoxDiv.childNodes[3]);
 
-        // //Create a new button in the package div to tell them the page failed to load data
-        // const otherButton = document.createElement('a');
-        // otherButton.className = "btn btn-purple btn-cartoon btn-cartoon-shadow btn-fill";
-        // otherButton.href = "https://register.centercentre.com/metricsvipwaitlist";
-        // otherButton.target = "_blank";
+    //     // //Create a new button in the package div to tell them the page failed to load data
+    //     // const otherButton = document.createElement('a');
+    //     // otherButton.className = "btn btn-purple btn-cartoon btn-cartoon-shadow btn-fill";
+    //     // otherButton.href = "https://register.centercentre.com/metricsvipwaitlist";
+    //     // otherButton.target = "_blank";
 
-        // const otherDiv = document.createElement('div');
-        // otherDiv.id = "regular-text";
-        // otherDiv.className = "cta";
-        // otherDiv.innerText = "Too Many Requests\nRefresh the page";
+    //     // const otherDiv = document.createElement('div');
+    //     // otherDiv.id = "regular-text";
+    //     // otherDiv.className = "cta";
+    //     // otherDiv.innerText = "Too Many Requests\nRefresh the page";
 
-        // //The waiting list text is appended to button
-        // otherButton.appendChild(otherDiv);
-        // //Button appended to packageDiv
-        // purpleBoxDiv.appendChild(otherButton);
-    }
+    //     // //The waiting list text is appended to button
+    //     // otherButton.appendChild(otherDiv);
+    //     // //Button appended to packageDiv
+    //     // purpleBoxDiv.appendChild(otherButton);
+    // }
 };
 
 window.onload = (_evt) => {
